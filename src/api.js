9b63fd5d2f69c14b6bcdfd95a94d56decb27bff2
vesperyang <129925575+vesperyang@users.noreply.github.com>
2026-02-25 02:59:58 +1000

    initial commit - frontend

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = "https://backend-8z39.onrender.com";
 
 export const addPageView = async () => {
   const res = await fetch(`${BASE_URL}/pageview`, { method: "POST" });
